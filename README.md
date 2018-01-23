522c6d20f4ada6cdf4aab55a4309e515e7c22c44
KanoniPan <panevschi.ruslan@gmail.com>
2018-01-23 23:21:50 +0200

    added to new account from Darkis133

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kanoni
+JavaScript Development Environment from Pluralsight Course with Cory House
+"# EnvJS" 
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,7 @@
+//var chalk = require('chalk');
+import chalk from 'chalk';
+
+/* eslint-disable no-console */
+// или ниже добавить комментарий
+
+console.log(chalk.green('Starting app in dev mode...')); //eslint-disable-line no-console
